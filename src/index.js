42654395d35015557151aff060f8db500c69b9a5
Vladimir Kunats <vladimir.kunats@gmail.com>
2022-08-07 19:24:51 +0300

    feat:add file index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+module.exports = function towelSort (matrix) {
+    let result = [];
+    if(!matrix) return result;
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 === 0) { 
+        result = result.concat(matrix[i]) 
+      }
+      else {
+        result = result.concat(matrix[i].reverse())
+      }
+    }   
+    return result
+  }
